1719ccb71b3906aaef9170935016b2711e84adda
David-Mcgaughey <napierdavid4@gmail.com>
2025-06-03 16:18:38 -0700

    second change - another test

--- PriceExtraction.py
+++ PriceExtraction.py
@@ -2,22 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-# Step 1: Get all product URLs from the Science category page
 base_url = "http://books.toscrape.com/"
 category_url = "http://books.toscrape.com/catalogue/category/books/science_22/index.html"
 response = requests.get(category_url)
 soup = BeautifulSoup(response.text, "html.parser")
 
-# Collect all book URLs from this page
+# Collect all book URLs from category page and store in list
 book_links = soup.select("h3 a")
 book_urls = [base_url + "catalogue/" + link["href"].replace("../", "") for link in book_links]
 
-# Prepare the CSV file
+# Write to the CSV file
 with open("science_books.csv", mode="w", newline="", encoding="utf-8") as file:
     writer = csv.writer(file)
     writer.writerow(["URL", "Title", "UPC", "Price (incl. tax)", "Price (excl. tax)", "Availability", "Description", "Category", "Rating", "Image URL"])
 
-    # Step 2: Visit each product page and scrape the info
+    # Visit each product page and scrape the info
     for url in book_urls:
         book_response = requests.get(url)
         book_soup = BeautifulSoup(book_response.text, "html.parser")
