7504598cc7ebd4b36e89e44d7c1d0247e4deb7aa
David-Mcgaughey <napierdavid4@gmail.com>
2025-06-03 14:16:00 -0700

    second change - another test

--- PriceExtraction.py
+++ PriceExtraction.py
@@ -2,21 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-# Step 1: Get the webpage
+# Get the webpage
 url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 response = requests.get(url)
 html = response.text
-
-# Step 2: Turn HTML into soup
 soup = BeautifulSoup(html, "html.parser")
 
-# Step 3: Find book information
+# Find book information
 title = soup.h1.text
 
 table = soup.find("table", class_="table table-striped")
 rows = table.find_all("tr")
 
-# Make a simple dictionary from the table
+# Make a dictionary with the books UPC, Prices, Availability
 book_info = {}
 for row in rows:
     header = row.th.text
@@ -35,17 +33,18 @@ category = soup.find("ul", class_="breadcrumb").find_all("li")[2].text.strip()
 
 # Rating
 rating_tag = soup.find("p", class_="star-rating")
-rating = rating_tag["class"][1] if rating_tag else "No rating"
+rating = rating_tag["class"][1]
 
 # Image URL
 image_tag = soup.find("img")
 image_url = "http://books.toscrape.com/" + image_tag["src"].replace("../", "")
 
-# Step 4: Save the data to CSV
+# Step 3: Save the data to CSV
 with open("one_book.csv", mode="w", newline="", encoding="utf-8") as file:
     writer = csv.writer(file)
-    writer.writerow(["Title", "UPC", "Price (incl. tax)", "Price (excl. tax)", "Availability", "Description", "Category", "Rating", "Image URL"])
+    writer.writerow(["URL","Title", "UPC", "Price (incl. tax)", "Price (excl. tax)", "Availability", "Description", "Category", "Rating", "Image URL"])
     writer.writerow([
+        url,
         title,
         book_info.get("UPC", ""),
         book_info.get("Price (incl. tax)", ""),
